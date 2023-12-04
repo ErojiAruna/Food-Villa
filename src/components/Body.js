8ea803e85710f5c7c7c1bda5c577eb36e97c5b60
ErojiAruna <aruna.eroji@gamil.com>
2023-12-04 22:47:50 +0530

    Ep-9

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import RestaurantCard from './RestaurantCard';
 import Shimmer from './Shimmer';
 import { Link } from 'react-router-dom';
+import useOnlineStatus from '../utils/useOnlineStatus';
 
 const Body = () => {
   // Local State Variable - Super powerfull variable
@@ -30,6 +31,12 @@ const Body = () => {
     );
   };
 
+  const onlineStatus = useOnlineStatus();
+
+  if (onlineStatus === false)
+    return (
+      <h1>Looks like you're offline!! Please check your internet connection</h1>
+    );
   return listOfRestaurants?.length === 0 ? (
     <Shimmer />
   ) : (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { LOGO_URL } from '../utils/constants';
 import { Link } from 'react-router-dom';
+import useOnlineStatus from '../utils/useOnlineStatus';
+import Grocery from './Grocery';
 
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState('Login');
 
-  useEffect(() => {
-    console.log('useEffect called');
-  }, []);
+  const onlineStatus = useOnlineStatus();
 
   return (
     <div className="header">
@@ -16,6 +16,7 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
+          <li>Online Status: {onlineStatus ? '✅' : '🔴'}</li>
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -24,6 +25,9 @@ const Header = () => {
           </li>
           <li>
             <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/grocery">Grocery</Link>
           </li>
           <li>Cart</li>
           <button
